fe584c07fd45d94acdae776ed1fdad0b8d02c3a8
Michael Rakowski <m.rakowski1+git@gmail.com>
2013-02-14 19:57:36 -0500

    -Add an iPhone folder to the project to hold iPhone specific code -Add a stub for DRBMainFeedViewController that will be used to show the main feed

---
+++ Dribbbler/iPhone/ViewControllers/DRBMainFeedViewController.m
@@ -0,0 +1,30 @@
+//
+//  DRBMainFeedViewController.m
+//  Dribbbler
+//
+//  Created by Michael Rakowski on 2/14/13.
+//  Copyright (c) 2013 Michael Rakowski. All rights reserved.
+//
+
+#import "DRBMainFeedViewController.h"
+
+@interface DRBMainFeedViewController ()
+@end
+
+@implementation DRBMainFeedViewController
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
