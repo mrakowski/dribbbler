7266633e8736f76218f9e9176f0aa161dea70936
Michael Rakowski <m.rakowski1+git@gmail.com>
2013-02-14 19:37:06 -0500

    -Add empty Xcode project shell

---
+++ Dribbbler/DRBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DRBAppDelegate.h
+//  Dribbbler
+//
+//  Created by Michael Rakowski on 2/14/13.
+//  Copyright (c) 2013 Michael Rakowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DRBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
