fe8c623da48881a1e46f4df513fb3b706b6ffaf2
Michael Rakowski <m.rakowski1+git@gmail.com>
2013-02-14 20:48:55 -0500

    -Add a stub for DRBFeedView

---
+++ Dribbbler/iPhone/Views/DRBFeedView.h
@@ -0,0 +1,13 @@
+//
+//  DRBFeedView.h
+//  Dribbbler
+//
+//  Created by Michael Rakowski on 2/14/13.
+//  Copyright (c) 2013 Michael Rakowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DRBFeedView : UIView
+
+@end
---
+++ Dribbbler/iPhone/Views/DRBFeedView.m
@@ -0,0 +1,25 @@
+//
+//  DRBFeedView.m
+//  Dribbbler
+//
+//  Created by Michael Rakowski on 2/14/13.
+//  Copyright (c) 2013 Michael Rakowski. All rights reserved.
+//
+
+#import "DRBFeedView.h"
+
+@implementation DRBFeedView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+        // Initialization code
+    }
+    return self;
+}
+
+
+
+@end
