0afbb4d8e85ace7c777db99bb05f848b6c57a479
Michael Rakowski <m.rakowski1+git@gmail.com>
2013-02-17 16:55:47 -0500

    -Add a stub for DRBPlayerDetailViewController

---
+++ Dribbbler/iPhone/ViewControllers/DRBPlayerDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DRBPlayerDetailViewController.h
+//  Dribbbler
+//
+//  Created by Michael Rakowski on 2/17/13.
+//  Copyright (c) 2013 Michael Rakowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DRBPlayerDetailViewController : UIViewController
+
+@end
---
+++ Dribbbler/iPhone/ViewControllers/DRBPlayerDetailViewController.m
@@ -0,0 +1,30 @@
+//
+//  DRBPlayerDetailViewController.m
+//  Dribbbler
+//
+//  Created by Michael Rakowski on 2/17/13.
+//  Copyright (c) 2013 Michael Rakowski. All rights reserved.
+//
+
+#import "DRBPlayerDetailViewController.h"
+
+@interface DRBPlayerDetailViewController ()
+@end
+
+@implementation DRBPlayerDetailViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+}
+
+#pragma mark -
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
